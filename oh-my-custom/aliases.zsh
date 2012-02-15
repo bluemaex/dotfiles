b060b98d4aabe2325ce2a247717bf8f53a5e30ff
Max Stockner <mail@bluemaex.de>
2012-02-15 14:03:34 +0100

    update

--- oh-my-custom/aliases.zsh
+++ oh-my-custom/aliases.zsh
@@ -1,3 +1,6 @@
 alias edithosts='sudo vim /etc/hosts'
 
 alias serve="python -m SimpleHTTPServer"
+
+alias tailapache="multitail /var/log/apache2/access_log /var/log/apache2/error_log"
+alias tailvhosts="multitail /var/log/apache2/vhosts*"
--- oh-my-custom/functions.zsh
+++ oh-my-custom/functions.zsh
@@ -19,7 +19,7 @@ lsofgrep() {
 histgrep() {
   if [ ! -z $1 ] ; then
     echo "Grepping history for $1..."
-    cat ~/.bash_history | grep -i $1 | grep -v grep
+    cat ~/.zsh_history | grep -i $1 | grep -v grep
   else
     echo "!! Need name to grep for"
   fi
@@ -32,4 +32,13 @@ function chmod_fd {
 
 function sshs {
 	ssh $@ -t screen
+}
+
+function tailwww() {
+    if [ ! -z $1 ] ; then
+        echo "\ntail -f & grep apache logs for $1...\n"
+        tail -f /var/log/apache2/vhosts-${2-'error'}_log | grep -i $1
+    else
+         tail -f /var/log/apache2/vhosts-error_log
+    fi
 }
