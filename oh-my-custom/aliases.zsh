c54b807edce71e2deac04c39505272b3f869f6a9
Max Stockner <mstockner@sprylab.com>
2012-05-21 17:23:35 +0200

    Add latexmk config

--- oh-my-custom/aliases.zsh
+++ oh-my-custom/aliases.zsh
@@ -2,5 +2,7 @@ alias edithosts='sudo vim /etc/hosts'
 
 alias serve="python -m SimpleHTTPServer"
 
+alias latexmk-watch='latexmk -pdf -pvc'
+
 alias tailapache="multitail /var/log/apache2/access_log /var/log/apache2/error_log"
 alias tailvhosts="multitail /var/log/apache2/vhosts*"
