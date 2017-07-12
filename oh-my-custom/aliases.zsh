c5e1a1dc48d248c585fbb388e72a2609c91ae55e
Max Stockner <mail@bluemaex.de>
2017-07-12 23:40:40 +0200

    remove old stuff

--- oh-my-custom/aliases.zsh
+++
@@ -1,7 +0,0 @@
-alias edithosts='sudo vim /etc/hosts'
-
-alias latexmk-watch='latexmk -pdf -pvc'
-
-alias tailapache="multitail /var/log/apache2/access_log /var/log/apache2/error_log"
-alias tailvhosts="multitail /var/log/apache2/vhosts*"
-alias httpdump='sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E "Host\: .*|GET \/.*"'
--- oh-my-custom/plugins/hosts/hosts.plugin.zsh
+++
@@ -1,2 +0,0 @@
-test -r $ZSH_CUSTOM/hosts/$HOST.path.zsh && source $ZSH_CUSTOM/hosts/$HOST.path.zsh
-test -r $ZSH_CUSTOM/hosts/$HOST.zsh && source $ZSH_CUSTOM/hosts/$HOST.zsh
